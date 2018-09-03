61f3a24538735e1d48359e914aef3bedc37a2694
Ángel Silva <angelsilvasana@gmail.com>
2018-09-03 11:54:05 +0200

    Cambio en Característica1

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log("Mensaje!");
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
